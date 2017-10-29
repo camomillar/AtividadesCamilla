af09bcf0101fe3f6c369f6e9df4bdac3ae2a6051
Camilla <camilladealmeidaalves@gmail.com>
2017-10-29 18:51:50 -0300

    parte 2 trabalho 2

--- trabalho 2/index.js
+++ trabalho 2/index.js
@@ -1,68 +1,47 @@
-var amigos = ['milla','dandan', 'Kimberly', 'brendon'];
-var mensagens = [,
-    {
-        user: '',
-        mensagens: []
-    },
-    {
-        user: amigos[1],
-        mensagens: [
-            {
-                usuario: amigos[0],
-                msg: 'oi'
-            },
-            {
-                usuario: amigos[1],
-                msg: 'oi'
-            },
-            {
-                usuario: amigos[0],
-                msg: 'td bem?'
-            }
-        ]
-    },
-    {
-        user: amigos[2],
-        mensagens: [
-            {
-                usuario: amigos[2],
-                msg: 'oi'
-            },
-            {
-                usuario: amigos[0],
-                msg: 'oi'
-            },
-            {
-                usuario: amigos[2],
-                msg: 'td bem?'
-            }
-        ]
-    },
-    {
-        user: amigos[3],
-        mensagens: [
-            {
-                usuario: amigos[0],
-                msg: 'oi'
-            },
-            {
-                usuario: amigos[3],
-                msg: 'oi'
-            },
-            {
-                usuario: amigos[0],
-                msg: 'td bem?'
-            }
-        ]
-    }
-];
+var groups = [];
+var messages = [];
 
+
+function getGroups(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            groups = JSON.parse(this.responseText);
+            console.log(groups);
+            makeUL(groups);
+        }
+    };
+    xhttp.open('GET','http://rest.learncode.academy/api/camomillar/groups',true);
+    xhttp.send();
+}
+
+getGroups();
+
+function clearMessages() {
+    var list_m = document.getElementById('mensagem');
+    list_m.innerHTML = "";
+}
+
+function getMessages(groupid) {
+    messages = [];
+    clearMessages();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            messages = JSON.parse(this.responseText);
+            console.log(messages);
+            makeMsgs(messages);
+        }
+    };
+    xhttp.open('GET', 'http://rest.learncode.academy/api/camomillar/'+groupid, true);
+    xhttp.send();
+}
 
 
 function makeUL(array) {
     var list = document.getElementById('amigosList');
 
-    for(var i = 1; i < array.length; i++) {
+    for(var i = 0; i < array.length; i++) {
         var item = document.createElement('li');
         var link = document.createElement('a');
         var img = document.createElement('img');
@@ -70,25 +49,30 @@ function makeUL(array) {
         img.width = 24;
         img.height = 24;
         link.appendChild(img);
-        link.appendChild(document.createTextNode(' ' + array[i]));
-        link.onclick = makeMsgs(i+1);
+        link.appendChild(document.createTextNode(' ' + array[i].groupName));
+        link.onclick = (function () {
+            var currentI = i;
+            console.log(currentI);
+            return function(){
+                getMessages(array[currentI].groupID);
+                document.getElementById('nome-amigo').innerHTML = array[currentI].groupName;
+            };
+        })();
         item.appendChild(link);
         list.appendChild(item);
     }
     return list;
 }
 
-function makeMsgs(index){
+
+function makeMsgs(msg){
     var list_m = document.getElementById('mensagem');
-    console.log(mensagens[index]);
-    for (var j = 0; j < mensagens[index].mensagens.length; j++) {
+    for (var j = 0; j < msg.length; j++) {
         var h3 = document.createElement('h3');
-        h3.appendChild(document.createTextNode(mensagens[index].mensagens[j].usuario));
+        h3.appendChild(document.createTextNode(msg[j].userName));
         var p = document.createElement('p');
-        p.appendChild(document.createTextNode(mensagens[index].mensagens[j].msg));
+        p.appendChild(document.createTextNode(msg[j].message));
         list_m.appendChild(h3);
         list_m.appendChild(p);
     }
 }
-
-makeUL(amigos);
